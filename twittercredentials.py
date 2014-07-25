235e86c03645b4e951544b0919113800d9cfffd2
digistam <digistam@gmail.com>
2014-07-25 13:20:22 +0200

    edited twittercredentials.py

--- twittercredentials.py
+++ twittercredentials.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 # © 2014, Mark Stam digistam@gmail.com 
-# get your consumer and access keys from Twitter and fill 'm in 
-# more information: http://iag.me/socialmedia/how-to-create-a-twitter-app-in-8-easy-steps/
 
 CONSUMER_KEY = ''
 CONSUMER_SECRET = ''
