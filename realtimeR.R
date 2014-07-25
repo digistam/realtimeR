62b826cf62a6a25a06dc36036ee0a9289d00e671
digistam <digistam@gmail.com>
2014-07-25 10:39:36 +0200

    initial commit realtimeR.R

---
+++ realtimeR.R
@@ -0,0 +1,20 @@
+# realtimeR.R
+# Author: Mark Stam
+#
+
+connectSQL <- function(x) {
+  require("RSQLite")
+  drv <<- dbDriver("SQLite")
+  con <<- dbConnect(drv, x)
+  dbListTables(con)
+  #dbDisconnect(con)
+}
+
+queryTable <- function(x) {
+  q <- dbGetQuery(con, paste("SELECT * FROM ", x, "", sep=""))
+  DF <<- as.data.frame(q)
+}
+
+topUsers <- function(x) {
+  head(sort(table(DF$username),decreasing=T),10)
+}
