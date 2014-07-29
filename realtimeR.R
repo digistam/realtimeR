f11087b815d0b760f7b37d16c2276f157ca13e80
digistam <digistam@gmail.com>
2014-07-29 16:33:53 +0200

    text mining added

--- realtimeR.R
+++ realtimeR.R
@@ -19,11 +19,12 @@ topUsers <- function(x) {
   head(sort(table(DF$username),decreasing=T),10)
 }
 
+##-----------------------------------------------------------------------------------
 ## sentiment analysis
-hu.liu.pos = scan('positive-words.txt',what='character', comment.char=';')
-hu.liu.neg = scan('negative-words.txt',what='character', comment.char=';')
-pos.words = c(hu.liu.pos)
-neg.words = c(hu.liu.neg)
+## credits: http://www.inside-r.org/howto/mining-twitter-airline-consumer-sentiment
+##
+## ----------------------------------------------------------------------------------
+
 score.sentiment <- function(sentences, pos.words, neg.words, .progress='none')
 {
   require(plyr)
@@ -65,6 +66,85 @@ score.sentiment <- function(sentences, pos.words, neg.words, .progress='none')
   scores.df = data.frame(score=scores, text=sentences)
   return(scores.df)
 }
-
+hu.liu.pos = scan('positive-words.txt',what='character', comment.char=';')
+hu.liu.neg = scan('negative-words.txt',what='character', comment.char=';')
+pos.words = c(hu.liu.pos)
+neg.words = c(hu.liu.neg)
 tweet.scores = score.sentiment(DF$content, pos.words, neg.words, .progress='text')
+
+## subsetting neg tweets
+neg.tweets <- DF[tweet.scores == -4 | tweet.scores == -5,]
+## subsetting pos tweets
+pos.tweets <- DF[tweet.scores == 4 | tweet.scores == 5,]
+
+print(paste('percentage positive tweets: ',nrow(pos.tweets) / sum(nrow(DF)) * 100))
+print(paste('percentage negative tweets: ',nrow(neg.tweets) / sum(nrow(DF)) * 100))
+
+##-----------------------------------------------------------------------------------
+## Geographic analysis
+## credits: 
+##
+## ----------------------------------------------------------------------------------
+
+#splitGeo <- strsplit(DF$geo,",")
+#firstElement <- function(x){x[1]}
+#secondElement <- function(x){x[2]}
+#lat <- sapply(splitGeo,firstElement)
+#lat <- lat[!is.na(lat)]
+#lon <- sapply(splitGeo,secondElement)
+#lon <- lat[!is.na(lon)]
+#geo <- cbind(lat,lon)
+
+##-----------------------------------------------------------------------------------
+## Text mining
+## credits: http://heuristically.wordpress.com/2011/04/08/text-data-mining-twitter-r/
+##
+## A dendrogram (from Greek dendron "tree" and gramma "drawing") is a tree diagram 
+## frequently used to illustrate the arrangement of the clusters produced by hierarchical 
+## clustering. 
+## ----------------------------------------------------------------------------------
+
+install.packages('tm')
+require(tm)
+pos.tweets.corpus <- Corpus(VectorSource(pos.tweets$content))
+pos.tweets.corpus <- tm_map(pos.tweets.corpus, tolower)
+pos.tweets.corpus <- tm_map(pos.tweets.corpus, removePunctuation)
+pos.tweets.stopwords <- c(stopwords('english'))
+pos.tweets.corpus <- tm_map(pos.tweets.corpus, removeWords, pos.tweets.stopwords)
+pos.tweets.dtm <- TermDocumentMatrix(pos.tweets.corpus)
+pos.tweets.dtm
+findFreqTerms(pos.tweets.dtm, lowfreq=30) ## find frequent words
+findAssocs(pos.tweets.dtm, 'mh17', 0.20)
+pos.tweets.dtm2 <- removeSparseTerms(pos.tweets.dtm, sparse=0.95)
+pos.tweets.df <- as.data.frame(inspect(pos.tweets.dtm2))
+nrow(pos.tweets.df)
+ncol(pos.tweets.df)
+pos.tweets.df.scale <- scale(pos.tweets.df)
+pos.d <- dist(pos.tweets.df.scale, method = "euclidean") # distance matrix
+pos.fit <- hclust(pos.d, method="ward")
+plot(pos.fit) # display dendogram?
+groups <- cutree(pos.fit, k=5) # cut tree into 5 clusters
+# draw dendogram with red borders around the 5 clusters
+rect.hclust(pos.fit, k=5, border="red")
+
+neg.tweets.corpus <- Corpus(VectorSource(neg.tweets$content))
+neg.tweets.corpus <- tm_map(neg.tweets.corpus, tolower)
+neg.tweets.corpus <- tm_map(neg.tweets.corpus, removePunctuation)
+neg.tweets.stopwords <- c(stopwords('english')) ## add more if needed
+neg.tweets.corpus <- tm_map(neg.tweets.corpus, removeWords, neg.tweets.stopwords)
+neg.tweets.dtm <- TermDocumentMatrix(neg.tweets.corpus)
+neg.tweets.dtm ## run to see contents
+findFreqTerms(neg.tweets.dtm, lowfreq=30) ## find frequent words
+findAssocs(neg.tweets.dtm, 'mh17', 0.20)
+neg.tweets.dtm2 <- removeSparseTerms(neg.tweets.dtm, sparse=0.95)
+neg.tweets.df <- as.data.frame(inspect(neg.tweets.dtm2))
+nrow(neg.tweets.df)
+ncol(neg.tweets.df)
+neg.tweets.df.scale <- scale(neg.tweets.df)
+neg.d <- dist(neg.tweets.df.scale, method = "euclidean") # distance matrix
+neg.fit <- hclust(neg.d, method="ward")
+plot(neg.fit) # display dendogram?
+groups <- cutree(neg.fit, k=5) # cut tree into 5 clusters
+# draw dendogram with red borders around the 5 clusters
+rect.hclust(neg.fit, k=5, border="red")
 
